bd2e11fe9d57f8c514027b103fe7292a87b4359c
Elizabeth A Kari <eakari@live.com>
2013-09-08 18:33:27 -0700

    initial commit

---
+++ routing/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Routing::Application.config.secret_key_base = '0c54656065011f2ba0c96bfd9986270f119b97cadf81b33e429e30f28574434bdc5de097da1c32c00c93c64db78c5cf516a9ac1abc49636978ee6114ab68616b'
